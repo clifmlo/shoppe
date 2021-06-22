33d93e252d6464b95ffc0d242d8aebb7b17f69ea
Cliford <cliford.mlotshwa@gmail.com>
2021-06-22 18:56:12 +0200

    first build

---
+++ src/main/java/com/assessment/shoppe/controller/ShopController.java
@@ -0,0 +1,18 @@
+package com.assessment.shoppe.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class ShopController {
+
+	@RequestMapping("/")
+	public String welcome(HttpServletRequest req) {
+		
+		return "Welcome to points shopping servce!";
+	}
+}
+
